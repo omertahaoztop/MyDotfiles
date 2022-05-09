0572717eaaaba286c3014033e303cf5dfa3a2584
Ömer Taha Öztop <omertahaoztop@hotmail.com>
2022-05-09 18:43:02 +0300

    My Dotfiles

---
+++ bspwm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo pacman -Syu --needed git base-devel
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin && makepkg -si
+paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome fm6000 network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
+git clone  --depth 1
+cd bspwm-dotfiles
+cp -R .config/* ~/.config/
+chmod -R +x ~/.config/bspwm
+cp .zshrc ~
+cp .zshrc-personal ~
+mkdir ~/.local/bin
+cp -R .local/bin/* ~/.local/bin
+chmod -R +x ~/.local/bin
+betterlockscreen -u ~/.config/bspwm/backgrounds/evening-sky.png
+sudo systemctl enable betterlockscreen@$USER.service
+
