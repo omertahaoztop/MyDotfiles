e0f3b0e99b5318dc3be154a2339f7d07ae79bbed
Ömer Taha Öztop <omertahaoztop@hotmail.com>
2022-05-12 18:38:06 +0300

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -5,13 +5,13 @@ cd paru-bin && makepkg -si
 paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome fm6000 network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
 git clone https://github.com/omertahaoztop/MyDotfiles 
 cd bspwm-dotfiles
-cp -R .config/* ~/.config/
+\cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
-cp .zshrc ~
-cp .zshrc-personal ~
+\cp .zshrc ~
+\cp .zshrc-personal ~
 mkdir ~/.local/bin
-cp -R .local/bin/* ~/.local/bin
-chmod -R +x ~/.local/bin
+\cp -R .local/bin/* ~/.local/bin
+\chmod -R +x ~/.local/bin
 betterlockscreen -u ~/.config/bspwm/backgrounds/evening-sky.png
 sudo systemctl enable betterlockscreen@$USER.service
 
