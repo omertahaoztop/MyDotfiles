8e65c0e6fb7280087e36b88122b81c808cd3b505
Ömer Taha Öztop <omertahaoztop@hotmail.com>
2022-05-14 11:39:23 +0300

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -4,7 +4,7 @@ git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -si
 paru -S alacritty bspwm sxhkd feh polybar xorg-xsetroot xorg-xbacklight light pamixer picom-jonaburg-git dunst rofi flameshot ksuperkey nerd-fonts-jetbrains-mono polkit-gnome fm6000 network-manager-applet helix xfce4-power-manager betterlockscreen zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-zsh-git catppuccin-gtk-theme papirus-icon-theme --needed --noconfirm
 git clone https://github.com/omertahaoztop/MyDotfiles 
-cd bspwm-dotfiles
+cd MyDotfiles
 \cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
 \cp .zshrc ~
